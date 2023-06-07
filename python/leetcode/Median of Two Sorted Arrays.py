db2cffbf31baccd6d93d0464af279f399fe9cbd9
jochanmin <happyjarban@gmail.com>
2023-06-08 00:04:27 +0900

    solve(23-06-08): leetcode top 4 problem using py

---
+++ python/leetcode/Median of Two Sorted Arrays.py
@@ -0,0 +1,20 @@
+"""
+this solution is not O(log(n+m))
+I have to change this logic as O(log(n+m)).
+"""
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        result = []
+        i = 0
+        j = 0
+        while i<len(nums1) and j<len(nums2):
+            if nums1[i] <= nums2[j]:
+                result.append(nums1[i])
+                i += 1
+            else:
+                result.append(nums2[j])
+                j += 1
+        result = result + nums1[i:] + nums2[j:]
+        size = len(result)
+        return (result[size//2]) if size % 2 != 0 else (result[size//2 - 1] + result[(size//2)])/2
